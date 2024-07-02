6b42cc04f366d18b1a63d65d009f4d18b35ae1e8
Joe simpson <joe@egmrc.com>
2024-07-02 09:22:35 -0600

    Update listings collection

--- src/collections/Listings/hooks/updateLatAndLong.ts
+++ src/collections/Listings/hooks/updateLatAndLong.ts
@@ -1,10 +1,10 @@
 import type {BeforeChangeHook} from 'payload/dist/collections/config/types'
 
-export const updateLatAndLong: BeforeChangeHook = async ({data, originalDoc}) => {
-        if (data.street !== originalDoc.street
-        || data.city !== originalDoc.city
-        || data.state !== originalDoc.state
-        || data.zip !== originalDoc.zip) {
+export const updateLatAndLong: BeforeChangeHook = async ({data,operation, originalDoc}) => {
+        if (operation === "create" || data.street !== originalDoc?.street
+        || data.city !== originalDoc?.city
+        || data.state !== originalDoc?.state
+        || data.zip !== originalDoc?.zip) {
         try{
             const res = await fetch(`${process.env.PAYLOAD_PUBLIC_SERVER_URL}/api/listings/address-search`, {
                 body: JSON.stringify({"address": data.street + " " + data.city + " " + data.state + " " + data.zip}),
@@ -18,9 +18,7 @@ export const updateLatAndLong: BeforeChangeHook = async ({data, originalDoc}) =>
 
 
             if (json.addresses[0].confidence === "exact" && (json.addresses[0].latitude !== data.latitude || json.addresses[0].longitude === data.longitude)){
-                
-                console.log("ah")
-                
+
                 return {
                     ...data,
                     latitude: json.addresses[0].latitude,
